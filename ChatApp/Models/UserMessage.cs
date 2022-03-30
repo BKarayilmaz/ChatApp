a76cfe0eb2697f2485d262df536bfedc84237b2c
BKarayilmaz <63595909+BKarayilmaz@users.noreply.github.com>
2022-03-30 23:48:47 +0300

    Add SignalR Hub/Configure Startup/UserMessage Model

---
+++ ChatApp/Models/UserMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Models
+{
+    public class UserMessage
+    {
+        public string Username { get; set; }
+        public string Message { get; set; }
+        public bool CurrentUser { get; set; }
+        public DateTime DateSend { get; set; }
+    }
+}
